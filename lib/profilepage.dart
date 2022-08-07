b452de62a5d32bf14ae55d5aa939fdba5fd2aba2
BustamanteFlocer <morilangelo808@gmail.com>
2022-08-08 00:16:23 +0800

    first commit

---
+++ lib/profilepage.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  Widget textfield({@required hintText}) {
+    return Material(
+      elevation: 4,
+      shadowColor: Colors.grey,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              letterSpacing: 2,
+              color: Colors.black54,
+              fontWeight: FontWeight.bold,
+            ),
+            fillColor: Colors.white30,
+            filled: true,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: const Color.fromARGB(167, 5, 143, 40), 
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+                
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+           
+            children: [
+              
+              Container(
+                height: 530,
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+              
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                      const SizedBox(
+                      height: 55,
+                      width: double.infinity,
+                        child: Center(
+                          child: Text(
+                            "Scrapper Name",
+                            style: TextStyle(
+                              fontSize: 23,
+                              color: Colors.black,
+                            ),
+                          
+                          ),
+                          
+                        ),  
+                           
+                    ),
+                    
+                 
+                    textfield(
+                      hintText: 'Username',
+                    ),
+                    textfield(
+                      hintText: 'Email',
+                    ),
+                 
+                     SizedBox(
+                      height: 55,
+                      width: double.infinity,
+                      
+                        child: const Center(
+                          child: Text(
+                            "Update",
+                            style: TextStyle(
+                              fontSize: 23,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      
+                    ),
+                    SizedBox(
+                      height: 55,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Center(
+                          child: Text(
+                            "Update",
+                            style: TextStyle(
+                              fontSize: 23,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          CustomPaint(
+            painter: HeaderCurvedContainer(),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(10),
+                
+              ),
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/profile.jpg'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Edit button
+          Padding(
+            padding: const EdgeInsets.only(bottom: 380, left: 184),
+            child: CircleAvatar(
+              backgroundColor: Colors.black54,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderCurvedContainer extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = const Color.fromARGB(167, 5, 143, 40); 
+    Path path = Path()
+      ..relativeLineTo(0, 60)
+      ..quadraticBezierTo(size.width / 2, 180, size.width, 60)
+      ..relativeLineTo(0, -60)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
